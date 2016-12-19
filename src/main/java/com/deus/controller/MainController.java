772e7de0045de22aeb129a9bb54c7d13cf0f781a
Maksym Baboshyn <deus.maxim@gmail.com>
2016-12-19 19:24:30 +0200

    Added saving attributes in session.

--- src/main/java/com/deus/controller/MainController.java
+++ src/main/java/com/deus/controller/MainController.java
@@ -5,18 +5,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("userJSP")
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView main() {
+    public ModelAndView main(@ModelAttribute("userJSP") User user) {
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.addObject("userJSP", new User());
         modelAndView.setViewName("index");
 
         return modelAndView;
+    }
+
+    //    hack
+    @ModelAttribute("userJSP")
+    public User createUser() {
+        return new User();
     }
 
     @RequestMapping(value = "/check-user")
